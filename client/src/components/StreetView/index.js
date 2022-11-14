9b71ae7e060e591d99ee5c8bce3fe86282edad6a
Sang Oh <baul3oh@gmail.com>
2022-11-14 14:43:01 -0600

    d

--- client/src/components/StreetView/index.js
+++ client/src/components/StreetView/index.js
@@ -6,28 +6,27 @@ console.log(process.env);
 // import * dotenv from 'dotenv'
 // dotenv.config()
 
-function StreetView({rand}) {
+function StreetView({ rand }) {
   const { loading, data } = useQuery(QUERY_LOCATIONS);
-  const locations= data?.locations|| [];
+  const locations = data?.locations || [];
   //rand = Math.floor(Math.random() * 2);
-  if (loading) return (<div>loading...</div>)
+  if (loading) return <div>loading...</div>;
   // see https://developers.google.com/maps/documentation/javascript
   const googleMapsApiKey = process.env.REACT_APP_googleMapsApiKey;
   //rand = 0;
   let latitude = locations[rand].latitude;
-  console.log(latitude)
+  console.log(latitude);
   let longitude = locations[rand].longitude;
-  console.log(longitude)
+  console.log(longitude);
 
   //console.log(typeof(longitude))
   //console.log(locations[rand].latitude)
   // see https://developers.google.com/maps/documentation/javascript/3.exp/reference#StreetViewPanoramaOptions
   const streetViewPanoramaOptions = {
-    position: { lat: latitude, lng: longitude},
+    position: { lat: -13.165071, lng: -72.544715 },
     pov: { heading: 100, pitch: 0 },
     zoom: 1,
   };
-  
 
   return (
     <div
@@ -43,7 +42,7 @@ function StreetView({rand}) {
           top: "0px",
           left: "0px",
           height: "75px",
-          width: "200px",
+          width: "250px",
           position: "absolute",
           backgroundColor: "white",
           zIndex: "2",
