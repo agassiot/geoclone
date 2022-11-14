448ff9b98afedfbbee8047a15d6284a001c7f3cb
Sang Oh <baul3oh@gmail.com>
2022-11-14 14:18:14 -0600

    buttons validates

--- client/src/components/StreetView/index.js
+++ client/src/components/StreetView/index.js
@@ -1,33 +1,43 @@
 import ReactStreetview from "react-streetview";
 import { useQuery } from "@apollo/client";
 import { QUERY_LOCATIONS } from "../../utils/queries";
+import { useEffect, useState } from "react";
 
 console.log(process.env);
 // import * dotenv from 'dotenv'
 // dotenv.config()
 
-function StreetView({rand}) {
+function StreetView({ rand }) {
   const { loading, data } = useQuery(QUERY_LOCATIONS);
-  const locations= data?.locations|| [];
+  const [apiKey, setApiKey] = useState("");
+  const locations = data?.locations || [];
+  useEffect(async () => {
+    const response = await fetch("/googleMapAPI");
+
+    const apiKey = await response.json();
+
+    setApiKey(apiKey);
+  }, []);
   //rand = Math.floor(Math.random() * 2);
-  if (loading) return (<div>loading...</div>)
+  if (loading) return <div>loading...</div>;
   // see https://developers.google.com/maps/documentation/javascript
-  const googleMapsApiKey = process.env.REACT_APP_googleMapsApiKey;
+  const googleMapsApiKey = apiKey;
   //rand = 0;
   let latitude = locations[rand].latitude;
-  console.log(latitude)
+  console.log(latitude);
   let longitude = locations[rand].longitude;
-  console.log(longitude)
+  console.log(longitude);
 
   //console.log(typeof(longitude))
   //console.log(locations[rand].latitude)
   // see https://developers.google.com/maps/documentation/javascript/3.exp/reference#StreetViewPanoramaOptions
   const streetViewPanoramaOptions = {
-    position: { lat: latitude, lng: longitude},
+    position: { lat: latitude, lng: longitude },
     pov: { heading: 100, pitch: 0 },
     zoom: 1,
   };
-  
+
+  if (!apiKey) return null;
 
   return (
     <div
@@ -43,7 +53,7 @@ function StreetView({rand}) {
           top: "0px",
           left: "0px",
           height: "75px",
-          width: "200px",
+          width: "250px",
           position: "absolute",
           backgroundColor: "white",
           zIndex: "2",
