d04a8642fffb34766c009b7d750f3c653171de08
Andrew Gassiot <gassiot.andrew@gmail.com>
2022-11-15 06:37:25 -0600

    merge

--- client/src/pages/ClickToPlay.js
+++ client/src/pages/ClickToPlay.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ClickToPlay = () => {
   return (
     <main>
-      <Link className="btn btn-lg btn-info m-2" to="/playing">
+      <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" to="/playing">
         CLICK HERE TO PLAY!
       </Link>
     </main>
